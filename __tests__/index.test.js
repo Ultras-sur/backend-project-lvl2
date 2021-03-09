588d901977d1a247985988ec9a38eab726974085
Ultras <ultras-sur@mail.ru>
2021-03-09 16:16:11 +0300

    add new tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,8 +1,8 @@
 import path from 'path';
 import compare from '../src/index.js';
 
-test('newtest', () => {
-  const expect2 =
+test('test1', () => {
+  const expect1 =
     `{\n` +
     `  - follow: false\n` +
     `    host: hexlet.io\n` +
@@ -11,10 +11,29 @@ test('newtest', () => {
     `  + timeout: 20\n` +
     `  + verbose: true\n` +
     `}`;
+
   expect(
     compare(
       path.resolve('./__fixtures__/before.json'),
       path.resolve('./__fixtures__/after.json')
+    )
+  ).toEqual(expect1);
+});
+
+test('test2', () => {
+  const expect2 =
+    `{\n` +
+    `  + follow: false\n` +
+    `    host: hexlet.io\n` +
+    `  + proxy: 123.234.53.22\n` +
+    `  - timeout: 20\n` +
+    `  + timeout: 50\n` +
+    `  - verbose: true\n` +
+    `}`;
+  expect(
+    compare(
+      path.resolve('./__fixtures__/after.json'),
+      path.resolve('./__fixtures__/before.json')
     )
   ).toEqual(expect2);
 });
